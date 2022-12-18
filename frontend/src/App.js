67167133d4b23c857f19e832b9c00159de2c0d75
Jinwoo Jung <wlsdn2749@gmail.com>
2022-12-18 11:03:08 +0000

    Update frontend login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import {Routes, Route} from "react-router-dom";
+import Login from "./Login";
+import Profile from "./Profile";
+import { RequireToken } from "./Auth";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <Routes> 
+        <Route path ="/" element = {<Login/>}/>
+        {/* <Route path ="/register" element = {</} */}
+        <Route 
+          path ="/profile"
+          element = {
+            <RequireToken>
+              <Profile/>
+            </RequireToken>
+          }
+        />
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/Login.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { fetchToken, setToken } from "./Auth";
+import axios from "axios";
+
+export default function Login(){
+    const navigate = useNavigate();
+    // what is Navigate?
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    // check to see if the fields are not empty
+    const login = () => {
+       if((username == "") & (password == "")){
+        return;
+       } else {
+        // make api call to our backend..
+        axios
+        .post("http://localhost:8000/login",{
+            username: username,
+            password: password,
+        })
+        .then(function (response) {
+            console.log(response.data.token, "response.data.token");
+            if (response.data.token){
+                setToken(response.data.token);
+                navigate("/profile");
+            }
+        })
+        .catch(function (error){
+            console.log(error, "error");
+        })
+       }
+    };
+
+    return (
+        <>
+            <div style={{ minHeight: 800, marginTop: 30}}>
+                <h1>login page</h1>
+                <div style={{ marginTop: 30}}>
+                    {fetchToken() ? (
+                        <p>you are loggin in</p>
+                    ) : (
+                        <div>
+                            <form>
+                                <label style={{ marginRight: 10}}>Input Username</label>
+                                <input
+                                    type="text"
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
+
+                                <label style={{ marginRight: 10}}>Input Password</label>
+                                <input
+                                    type="text"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+
+                                <button onClick={login}>Login</button>
+                            </form>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </>
+    );
+}
